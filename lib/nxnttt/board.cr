d98a066392e45f8a1e947f3cd513f765dba09081
Collided Scope <collidedscope+github@protonmail.com>
2021-07-10 00:59:00 -0400

    Add `rows` and `columns` caching getters to `Board`

--- lib/nxnttt/board.cr
+++ lib/nxnttt/board.cr
@@ -9,6 +9,16 @@ struct NxNTTT::Board
     (1..n).sum { |i| 1u128 << n * i - i }
   end
 
+  getter rows : Array(UInt128) do
+    Array.new(n) { |i| (1u128 << n) - 1 << n * i }
+  end
+
+  getter columns : Array(UInt128) do
+    Array.new(n) { |i|
+      (0...n).sum { |j| 1u128 << j * n + i }
+    }
+  end
+
   def initialize(@n)
     raise "board size must be 1 <= N <= 11" unless 1 <= n <= 11
   end
