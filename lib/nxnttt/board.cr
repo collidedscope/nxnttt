a025b2cdaa1255e903dec6e9c5c3f21d0b60983f
Collided Scope <collidedscope+github@protonmail.com>
2021-07-10 00:58:10 -0400

    Prefer exclusive range

--- lib/nxnttt/board.cr
+++ lib/nxnttt/board.cr
@@ -2,7 +2,7 @@ struct NxNTTT::Board
   getter n : Int32, marks : UInt128 = 0, occupancy : UInt128 = 0
 
   getter diagonal : UInt128 do
-    (1..n - 1).sum { |i| 1u128 << n * i + i } + 1
+    (1...n).sum { |i| 1u128 << n * i + i } + 1
   end
 
   getter antidiagonal : UInt128 do
@@ -13,4 +13,3 @@ struct NxNTTT::Board
     raise "board size must be 1 <= N <= 11" unless 1 <= n <= 11
   end
 end
-
